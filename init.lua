ea2395e7f0d3d994e5dbf3c22d59b2daf155d0ac
Beha <shacknetisp@mail.com>
2020-04-09 10:57:20 -0400

    Agnostify

--- init.lua
+++ init.lua
@@ -46,23 +46,30 @@ if creative_mode_cache then
 	local digtime = 42
 	local caps = {times = {digtime, digtime, digtime}, uses = 0, maxlevel = 256}
 
+	local def = minetest.registered_items[""]
+
+	local groupcaps = {
+		-- dig_immediate group doesn't use value 1. Value 3 is instant dig
+		dig_immediate =
+			{times = {[2] = digtime, [3] = 0}, uses = 0, maxlevel = 256},
+	}
+	for k in pairs(def.tool_capabilities.groupcaps) do
+		groupcaps[k] = caps
+	end
+
+	local damage_groups = {}
+	for k,v in pairs(def.tool_capabilities.damage_groups) do
+		damage_groups[k] = v * 10
+	end
+
 	-- Override the hand tool
 	minetest.override_item("", {
 		range = 10,
 		tool_capabilities = {
 			full_punch_interval = 0.5,
 			max_drop_level = 3,
-			groupcaps = {
-				crumbly = caps,
-				cracky  = caps,
-				snappy  = caps,
-				choppy  = caps,
-				oddly_breakable_by_hand = caps,
-				-- dig_immediate group doesn't use value 1. Value 3 is instant dig
-				dig_immediate =
-					{times = {[2] = digtime, [3] = 0}, uses = 0, maxlevel = 256},
-			},
-			damage_groups = {fleshy = 10},
+			groupcaps = groupcaps,
+			damage_groups = damage_groups,
 		}
 	})
 end
